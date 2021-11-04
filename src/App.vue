c450bb1436e41503d00eaef9a26830adad772def
rizwanullah313 <ru323091@gmail.com>
2021-11-04 18:35:45 +0000

    Create blog using vue js

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+  <add-blog></add-blog>
+  </div>
+</template>
+
+<script>
+import Addblog from "./components/Addblog.vue";
+export default {
+  components: {
+      'add-blog': Addblog
+ },
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style>
+</style>
